603528eb88ecc9d2d3eca8faa1546c9d8c0bfb03
dmitars <dmitriy.tarasienko@gmail.com>
2020-07-13 12:59:36 +0300

    refactor: add standard project files and resources

---
+++ src/test/java/com/example/springExample/SpringWebSiteExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSiteExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
