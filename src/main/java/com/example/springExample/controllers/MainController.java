4cde5a534ca023d573383d8a6bbceb071eb059b0
dmitars <dmitriy.tarasienko@gmail.com>
2020-07-13 13:19:54 +0300

    fix: solve bag with protecting current action

--- src/main/java/com/example/springExample/controllers/MainController.java
+++ src/main/java/com/example/springExample/controllers/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -27,6 +28,11 @@ public class MainController {
 
     @GetMapping("/access-denied")
     public String blockAccess(Model model){
+        return "access-denied";
+    }
+
+    @PostMapping("/access-denied")
+    public String actionAccess(Model model){
         return "access-denied";
     }
 }
