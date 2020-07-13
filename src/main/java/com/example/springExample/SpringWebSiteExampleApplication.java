138ff4c5fafa3f4c1f9b3d721f3c1c415e13b093
dmitars <dmitriy.tarasienko@gmail.com>
2020-07-13 12:54:04 +0300

    feat: add main function

---
+++ src/main/java/com/example/springExample/SpringWebSiteExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSiteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSiteExampleApplication.class, args);
+	}
+
+}
