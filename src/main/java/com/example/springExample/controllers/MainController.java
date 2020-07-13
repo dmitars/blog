0ab88aedecd0cadcc7053b99d91b7b529aa0f92b
dmitars <dmitriy.tarasienko@gmail.com>
2020-07-13 11:47:52 +0300

    feat: create main controller

---
+++ src/main/java/com/example/springExample/controllers/MainController.java
@@ -0,0 +1,32 @@
+package com.example.springExample.controllers;
+
+import com.example.springExample.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Controller
+public class MainController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        var posts = postRepository.findAllChecked();
+        var list = StreamSupport.stream(posts.spliterator(),false).collect(Collectors.toList());
+        if(list.size()>15)
+            list = list.subList(0,15);
+        model.addAttribute("posts",list);
+        return "home";
+    }
+
+    @GetMapping("/access-denied")
+    public String blockAccess(Model model){
+        return "access-denied";
+    }
+}
